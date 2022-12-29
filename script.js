3e88483039173c979e8f980ddcfa5baabd0b9f3d
Felipe_Fastrone <ffastrone@gmail.com>
2022-12-29 14:25:52 -0300

    commit

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const buttonOpen = document.querySelector('.open-menu');
 const buttonClose = document.querySelector('close-menu');
 
-
-const openMenu = () => {
-    buttonOpen.innerHTML.style.display = 'hidden';
-
-}
-const closeMenu = () => {
-    
-}
-
 buttonOpen.addEventListener('click', openMenu);
 buttonClose.addEventListener('click', closeMenu);
+
+function openMenu() {
+    document.body.classList.add('menuActive');
+}
+function closeMenu() {
+    document.body.classList.remove('menuActive')
+}
+
+openMenu();
+closeMenu();
